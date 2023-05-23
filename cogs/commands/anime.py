9d3bf76fef3ada47c579da46f614292ed0bbec62
BernardWong97 <ben.wc88@gmail.com>
2023-05-23 10:37:10 +0800

    Anime command partially done with pagination. Added Enums for anime command response.

---
+++ cogs/commands/anime.py
@@ -0,0 +1,191 @@
+import os, discord, requests, html2markdown, urllib.parse
+from datetime import datetime, timezone, timedelta
+from discord import Embed, ApplicationContext, Option
+from discord.ext.commands import Cog, Bot
+from utilities.enums import MediaType, MediaFormat, MediaSeason, MediaSource, MediaStatus, Country
+
+class Anime(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+        self.API_URL = 'https://graphql.anilist.co'
+        self.WEB_URL = 'https://anilist.co'
+
+    @discord.slash_command(name="anime", description="List search anime", guild_ids=[int(os.getenv('TEST_GUILD'))], guild_only=True)
+    async def list_anime(self, 
+                         ctx: ApplicationContext, 
+                         search: Option(str, description="Filter by search query", required=False),
+                         season: Option(str, description="Filter by the season the anime was released in", choices=MediaSeason.get_choices(), required=False),
+                         season_year: Option(int, description="The year of the season. Requires season argument", required=False),
+                         format: Option(str, description="Filter by the anime's format", choices=MediaFormat.get_choices(), required=False),
+                         status: Option(str, description="Filter by the anime's current release status", choices=MediaStatus.get_choices(), required=False),
+                         country: Option(str, description="Filter by the anime's country of origin (ISO 3166-1 alpha-2)", required=False),
+                         nsfw: Option(bool, description="Filter by if the anime's intended for 18+ adult audiences", default=False),
+                         genre: Option(str, description="Filter by anime's genre", required=False),
+                         source: Option(str, description="Filter by the anime's format", choices=MediaSource.get_choices(), required=False),
+                         ):
+        # await ctx.defer()
+
+        # data = await self.retrieve_anime_list(search)
+
+        # if "errors" in data:
+        #     print(data)
+        #     await ctx.followup.send("There's something wrong with the command")
+        # else:
+        #     media_list = data["media"]
+
+        #     embed_list = self._generate_embeds(search, media_list)
+
+        #     paginator = Paginator(pages=embed_list)
+        #     await paginator.respond(ctx.interaction, ephemeral=False)
+        await ctx.respond(season + " " + str(season_year))
+
+    async def retrieve_anime_list(self, search: str):
+        query = '''
+        query ($page: Int, $perPage: Int, $search: String, $mediaType: MediaType) {
+            Page (page: $page, perPage: $perPage) {
+                pageInfo {
+                    total
+                    perPage
+                    currentPage
+                    lastPage
+                    hasNextPage
+                }
+                media(search: $search, type: $mediaType) {
+                    format
+                    status(version: 2)
+                    description
+                    season
+                    episodes
+                    duration
+                    countryOfOrigin
+                    source(version: 3)
+                    genres
+                    averageScore
+                    meanScore
+                    popularity
+                    trending
+                    favourites
+                    isAdult
+                    siteUrl
+                    title {
+                        romaji(stylised: true)
+                        english(stylised: true)
+                        native(stylised: true)
+                    }
+                    startDate {
+                        year
+                        month
+                        day
+                    }
+                    endDate {
+                        year
+                        month
+                        day
+                    }
+                    trailer {
+                        id
+                        site
+                    }
+                    coverImage {
+                        extraLarge
+                        large
+                        medium
+                        color
+                    }
+                    studios(isMain: true) {
+                        nodes {
+                            name
+                            siteUrl
+                        }
+                    }
+                }
+            }
+        }
+        '''
+        variables = {
+            'search': search,
+            'mediaType': MediaType.ANIME.name,
+            'page': 1,
+            'perPage': 10
+        }
+
+        response = requests.post(self.API_URL, json={'query': query, 'variables': variables})
+
+        data = response.json()
+
+        if "errors" in data:
+            return data
+        else:
+            return data["data"]["Page"]
+    
+    def _generate_embeds(self, search: str, media_list) -> list[Embed]:
+        embed_list = []
+
+        title_embed = Embed(
+            title = f'{search} Anime Search Result',
+            color = 0x0000FF,
+            url = f'{self.WEB_URL}/search/anime?search={urllib.parse.quote(search)}'
+        ).set_author(name = "AniList", icon_url="https://avatars.githubusercontent.com/u/18018524?s=200&v=4")
+
+        for media in media_list:
+            romanji = media["title"]["romaji"]
+            english = media["title"]["english"]
+
+            description = f"{romanji}\n{english}\n\n" if romanji and english else english or ""
+            description += f"\n\n{html2markdown.convert(media['description'])}"
+
+            embed = Embed(
+                title = media["title"]["native"] or media["title"]["romanji"] or media["title"]["english"],
+                colour = int(media["coverImage"]["color"][1:], 16) if media["coverImage"]["color"] else 0xFF00FF,
+                url = media["siteUrl"],
+                description = description
+            )
+
+            if (len(media["studios"]["nodes"]) != 0):
+                embed.set_author(name=media["studios"]["nodes"][0]["name"] or "Unknown", url=media["studios"]["nodes"][0]["siteUrl"] or "")
+            else:
+                embed.set_author(name="Unknown")
+
+            if media["coverImage"]["medium"]:
+                embed.set_thumbnail(url=media["coverImage"]["medium"])
+
+            embed.set_image(url=media["coverImage"]["extraLarge"] or media["coverImage"]["large"] or media["coverImage"]["medium"])
+
+            embed.add_field(name="Episodes", value=media["episodes"], inline=True)
+            embed.add_field(name="", value="", inline=True)
+            embed.add_field(name="Duration", value=f'{media["duration"]} min', inline=True)
+            embed.add_field(name="Format", value= MediaFormat.get_value(media["format"]), inline=True)
+            embed.add_field(name="Country", value= Country.get_value(media["countryOfOrigin"]), inline=True)
+            embed.add_field(name="Status", value= MediaStatus.get_value(media["status"]), inline=True)
+            embed.add_field(name="Season", value= MediaSeason.get_value(media["season"]), inline=True)
+            embed.add_field(name="Source", value= MediaSource.get_value(media["source"]), inline=True)
+            embed.add_field(name="Average Score", value= f'{media["averageScore"]}%', inline=True)
+            embed.add_field(name="Mean Score", value= f'{media["meanScore"]}%', inline=True)
+            embed.add_field(name="Popularity", value= media["popularity"], inline=True)
+            embed.add_field(name="NSFW", value= "Yes" if media["isAdult"] else "No", inline=True)
+
+            embed.add_field(name="Genre", value=", ".join(media["genres"]), inline=False)
+
+            start_date = self._format_date(media["startDate"])
+            end_date = self._format_date(media["endDate"])
+
+            embed.set_footer(text=f"{start_date} - {end_date}")
+
+            if media["trailer"] and media["trailer"]["site"] == "youtube":
+                video_url = f'https://www.youtube.com/watch?v={media["trailer"]["id"]}'
+                embed.add_field(name="Trailer", value=f"[Youtube]({video_url})")
+
+            embed_list.append([title_embed, embed])
+
+        return embed_list
+
+    def _format_date(self, date_json) -> str:
+        day = f'{date_json["day"]:02d}' if date_json["day"] else "-"
+        month = f'{date_json["month"]:02d}' if date_json["month"] else "-"
+        year = f'{date_json["year"]:02d}' if date_json["year"] else "-"
+
+        date_str = f"{day}/{month}/{year}"
+        return date_str if date_str != '-/-/-' else 'Unknown'
+
+def setup(bot):
+    bot.add_cog(Anime(bot))
---
+++ utilities/enums.py
@@ -0,0 +1,88 @@
+import pycountry
+from discord import OptionChoice
+from datetime import datetime, timezone, timedelta
+from enum import Enum, auto
+
+class BaseEnum():
+    @classmethod
+    def get_value(clazz: object, name: str) -> str:
+        for member in clazz:
+            if name and member.name == name.upper():
+                if isinstance(member.value, int):
+                    return member.name.replace('_', ' ').title()
+                else:
+                    return member.value
+        return "Unknown"
+
+    @classmethod
+    def get_choices(clazz: object) -> list[OptionChoice]:
+        return [OptionChoice(name=clazz.get_value(member.name)) for member in clazz]
+
+class MediaType(BaseEnum, Enum):
+    ANIME = auto()
+    MANGA = auto()
+
+class MediaFormat(BaseEnum, Enum):
+    TV = "TV"
+    TV_SHORT = "TV Short"
+    MOVIE = auto()
+    SPECIAL = auto()
+    OVA = "OVA"
+    ONA = "ONA"
+    MUSIC = auto()
+    MANGA = auto()
+    NOVEL = auto()
+    ONE_SHOT = auto()
+
+class MediaStatus(BaseEnum, Enum):
+    FINISHED = auto()
+    RELEASING = "On Going"
+    NOT_YET_RELEASED = auto()
+    CANCELLED = auto()
+    HIATUS = auto()
+
+class MediaSeason(BaseEnum, Enum):
+    WINTER = auto()
+    SPRING = auto()
+    SUMMER = auto()
+    FALL = auto()
+
+    @staticmethod
+    def get_current_season() -> str:
+        tz = timezone(timedelta(hours=8), name='Asia/Kuala_Lumpur')
+        current_month = datetime.now(tz).month
+
+        if current_month in [12, 1, 2]:
+            return MediaSeason.get_value(MediaSeason.WINTER.name)
+        elif current_month in [3, 4, 5]:
+            return MediaSeason.get_value(MediaSeason.SPRING.name)
+        elif current_month in [6, 7, 8]:
+            return MediaSeason.get_value(MediaSeason.SUMMER.name)
+        else:
+            return MediaSeason.get_value(MediaSeason.FALL.name)
+
+class MediaSource(BaseEnum, Enum):
+    ORIGINAL = auto()
+    MANGA = auto()
+    LIGHT_NOVEL = auto()
+    VISUAL_NOVEL = auto()
+    VIDEO_GAME = auto()
+    OTHER = auto()
+    NOVEL = auto()
+    DOUJINSHI = auto()
+    ANIME = auto()
+    WEB_NOVEL = auto()
+    LIVE_ACTION = auto()
+    GAME = auto()
+    COMIC = auto()
+    MULTIMEDIA_PROJECT = auto()
+    PICTURE_BOOK = auto()
+
+class Country():
+    @staticmethod
+    def get_value(name: str) -> str:
+        country = pycountry.countries.get(alpha_2=name)
+
+        if country is None:
+            return "Unknown"
+        return country.name
