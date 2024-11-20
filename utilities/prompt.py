7e223656e96c8cbc5fd978b45ebf6a291ca53e9d
BernardWong97 <ben.wc88@gmail.com>
2024-11-20 11:50:21 +0800

    Fix prompt

--- utilities/prompt.py
+++ utilities/prompt.py
@@ -12,7 +12,7 @@ async def prompt(bot: Bot, message: Message) -> str:
         }
     }
 
-    role_message = "Reply like you are willing to help."
+    role_message = "Reply like a friend."
 
     result = await retrieve_data(f'SELECT * FROM {os.getenv("MYSQL_QUOTE_TABLE")} WHERE member_id = {message.author.id};')
 
@@ -27,11 +27,18 @@ async def prompt(bot: Bot, message: Message) -> str:
     result = await retrieve_data(f'SELECT role_message FROM {os.getenv("MYSQL_MEMBER_TABLE")} WHERE id = {message.author.id};')
 
     if len(result) > 0:
-        role_message = result[0][0]
+        if (result[0][0] is not None):
+            role_message = result[0][0]
+
+    role_message += f" Here are some examples that you will reply: "
 
     if len(quote_messages) > 0:
-        role_message += f" Here are some examples that you will reply: "
         role_message += ", ".join(quote_messages)
+    else:
+        quote_array = ["Why do you tag me?", "什麽事情啊？", "叫我嗎?", "???", "做莫"]
+        role_message += ", ".join(quote_array)
+
+    print(role_message)
 
     input_messages = [HumanMessage(content=message.content)]
     output = bot.ai.invoke({"messages": input_messages, "role_message": role_message}, config)
